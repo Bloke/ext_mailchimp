c090702bbea0ed2fdd9f36975d42483d0b9c715f
Stef Dawson <thatbloke@stefdawson.com>
2017-07-19 09:18:55 +0100

    Semantic versioning

--- ext_mailchimp.php
+++ ext_mailchimp.php
@@ -17,9 +17,9 @@ $plugin['name'] = 'ext_mailchimp';
 // 1 = Plugin help is in raw HTML.  Not recommended.
 # $plugin['allow_html_help'] = 1;
 
-$plugin['version'] = '0.10';
+$plugin['version'] = '1.0.0';
 $plugin['author'] = 'Stef Dawson';
-$plugin['author_uri'] = 'http://stefdawson.com';
+$plugin['author_uri'] = 'https://stefdawson.com';
 $plugin['description'] = 'Textpattern CMS Mailchimp module for the com_connect email plugin';
 
 // Plugin load order:
